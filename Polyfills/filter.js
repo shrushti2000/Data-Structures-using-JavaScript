d904b231fd3029d04b6f9da9a1ff2c7f597c81d0
Shrushti Polekar <shrushtipolekar@gmail.com>
2022-07-19 15:48:28 +0000

    filter polfill

--- Polyfills/filter.js
+++ Polyfills/filter.js
@@ -2,10 +2,12 @@ const arr=[2,3,4,5,6,8,9];
 Array.prototype.myMap=function(cb){
   let temp=[];
   for(let i=0;i<this.length;i++){
-    temp.push(cb(i,this[i],this))
+    if(cb(i,this[i],this)){
+      temp.push(this[i])
     }
+  }
   return temp;
 }
 
-const newArr=arr.myMap((i,item,arr)=>item*2 );
+const newArr=arr.myMap((i,item,arr)=>item%2==0 );
 console.log(newArr)
